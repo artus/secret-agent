a4f9d1f141072f3b733f470083b696ee124b524b
artusvranken <artusvranken@gmail.com>
2017-11-30 13:09:14 +0100

    Website nearly done.

---
+++ secret-agent.js
@@ -0,0 +1,10 @@
+// The browser script for the Secret Agent assignment.
+
+var txtFirstPart = document.getElementById('firstPart');
+var txtSecondPart = document.getElementById('secondPart');
+var txtThirdPart = document.getElementById('thirdPart');
+var txtFourthPart = document.getElementById('fourthPart');
+
+var decrypt = new secret_agent.Decrypt();
+
+var txtSolution = document.getElementById('solution');
